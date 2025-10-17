ec88242c772c54bd23ff1651eef51f2adf888f8f
Lance Romero <151988727+LanceRomero@users.noreply.github.com>
2025-10-17 12:14:22 +0800

    .

--- chatbot/mainmodule.js
+++ chatbot/mainmodule.js
@@ -1,6 +1,6 @@
 import {GoogleGenerativeAI} from "@google/generative-ai";
 
-const API_KEY = "AIzaSyDzLbIHsthqOkhlmJGnwxsHqHfD7i7wxkw";
+const API_KEY = "";
 const genAI = new GoogleGenerativeAI(API_KEY);
 const model = genAI.getGenerativeModel({
     model: "gemini-1.5-flash", 
